4663a8796dab164bca47493712e7b5de91e68a7b
Sebastian Radloff <sebastian.radloff@namely.com>
2021-08-20 17:51:20 -0400

    Switches to gatekeeper-policy-manager helm chart and removes resource requests for local use.

--- scripts/update_gatekeeper_infra_manifests.sh
+++ scripts/update_gatekeeper_infra_manifests.sh
@@ -26,25 +26,30 @@ docker run -it --rm -v /tmp:/apps -w /apps "${HELM_IMAGE}" \
     --untardir "${GATEKEEPER_HELM_CHART_DIR_NAME}" --untar "${GATEKEEPER_HELM_CHART_NAME}"
 
 manifest_content=$(docker run -it --rm -v /tmp:/apps -w /apps/ "${HELM_IMAGE}" \
-    template --include-crds --set replicas=1 --set auditInterval=10 --namespace gatekeeper-system \
+    template --include-crds --set replicas=1 --set auditInterval=10 --set controllerManager.resources=null \
+    --set audit.resources=null --namespace gatekeeper-system \
     "${GATEKEEPER_HELM_CHART_NAME}" "${GATEKEEPER_HELM_CHART_DIR_NAME}/${GATEKEEPER_HELM_CHART_NAME}")
 
 echo "${manifest_content}" > "${GATEKEEPER_OUTPUT_MANIFEST}"
 
 
 # template gatekeeper-policy-manager manifests
-GATEKEEPER_POLICY_MANAGER_RAW_URL="https://raw.githubusercontent.com/sighupio/gatekeeper-policy-manager"
-GATEKEEPER_POLICY_MANAGER_VERSION="v0.4.2"
-GATEKEEPER_POLICY_MANAGER_MANIFESTS_URL="${GATEKEEPER_POLICY_MANAGER_RAW_URL}/${GATEKEEPER_POLICY_MANAGER_VERSION}/manifests"
-GATEKEEPER_POLICY_MANAGER_NAME="gatekeeper-policy-manager"
-GATEKEEPER_POLICY_MANAGER_DIR="/tmp/${GATEKEEPER_POLICY_MANAGER_NAME}"
-GATEKEEPER_POLICY_MANAGER_OUTPUT_MANIFEST="${MANIFESTS_OUTPUT_DIR}/${GATEKEEPER_POLICY_MANAGER_NAME}.yml"
+GATEKEEPER_POLICY_MANAGER_VERSION="0.5.0"
+GATEKEEPER_POLICY_MANAGER_TAR_GZ_URL="https://github.com/sighupio/gatekeeper-policy-manager/archive/refs/tags/v${GATEKEEPER_POLICY_MANAGER_VERSION}.tar.gz"
+GATEKEEPER_POLICY_MANAGER_HELM_CHART_DIR_NAME="gatekeeper-policy-manager"
+GATEKEEPER_POLICY_MANAGER_DIR="/tmp/${GATEKEEPER_POLICY_MANAGER_HELM_CHART_DIR_NAME}"
+GATEKEEPER_POLICY_MANAGER_CHART_NAME="gatekeeper-policy-manager"
+GATEKEEPER_POLICY_MANAGER_OUTPUT_MANIFEST="${MANIFESTS_OUTPUT_DIR}/${GATEKEEPER_POLICY_MANAGER_CHART_NAME}.yml"
 
 rm -rf "${GATEKEEPER_POLICY_MANAGER_DIR}"
 mkdir -p "${GATEKEEPER_POLICY_MANAGER_DIR}"
 
-curl -sL "${GATEKEEPER_POLICY_MANAGER_MANIFESTS_URL}/rbac.yaml" >> "${GATEKEEPER_POLICY_MANAGER_OUTPUT_MANIFEST}"
-echo "---" >> "${GATEKEEPER_POLICY_MANAGER_OUTPUT_MANIFEST}"
-curl -sL "${GATEKEEPER_POLICY_MANAGER_MANIFESTS_URL}/deployment.yaml" | sed "s/quay.io\/sighup\/gatekeeper-policy-manager/quay.io\/sighup\/gatekeeper-policy-manager:${GATEKEEPER_POLICY_MANAGER_VERSION}/" >> "${GATEKEEPER_POLICY_MANAGER_OUTPUT_MANIFEST}"
-echo "---" >> "${GATEKEEPER_POLICY_MANAGER_OUTPUT_MANIFEST}"
-curl -sL "${GATEKEEPER_POLICY_MANAGER_MANIFESTS_URL}/service.yaml" >> "${GATEKEEPER_POLICY_MANAGER_OUTPUT_MANIFEST}"
+curl -sL "${GATEKEEPER_POLICY_MANAGER_TAR_GZ_URL}" | tar -xf - \
+    --strip-components=2 -C "${GATEKEEPER_POLICY_MANAGER_DIR}" \
+    "${GATEKEEPER_POLICY_MANAGER_CHART_NAME}-${GATEKEEPER_POLICY_MANAGER_VERSION}/chart/" 
+
+manifest_content=$(docker run -it --rm -v /tmp:/apps -w /apps/ "${HELM_IMAGE}" \
+    template --namespace gatekeeper-system --set config.secretKey="test" --set replicaCount=1 --set resources=null \
+    "${GATEKEEPER_POLICY_MANAGER_CHART_NAME}" "${GATEKEEPER_POLICY_MANAGER_HELM_CHART_DIR_NAME}")
+
+echo "${manifest_content}" > "${GATEKEEPER_POLICY_MANAGER_OUTPUT_MANIFEST}"
